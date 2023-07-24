0d84ededf8d39a17ca903c3f61e0404b48b6b408
Ian Panganiban <ian.panganiban@outlook.ph>
2023-07-24 20:25:40 +0800

    chore: removed extra print logs.

--- api/gateway/routers/geocoder.py
+++ api/gateway/routers/geocoder.py
@@ -12,7 +12,5 @@ async def geocode_url(q: Union[str, None] = None):
     if q is None:
         return {"message": geocode_url}
     query_params = {"q": q}
-    print(geocode_url, query_params)
     r = requests.get(geocode_url, params=query_params)
-    print(r.text, r.status_code)
     return r.json()
