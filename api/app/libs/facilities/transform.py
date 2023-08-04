b183d7a04d7c95536590e409f1c25d3f44823f09
Ian Panganiban <ian.panganiban@outlook.ph>
2023-08-04 11:56:11 +0800

    chore: created data loader script and update data model to version 08042023

--- api/app/libs/facilities/transform.py
+++ api/app/libs/facilities/transform.py
@@ -1,15 +1,16 @@
 def transform_es_result_to_geojson(es_queries):
     """
-     Transforms ES results into GeoJSON. This is a helper function to be used by the geospatial_search module.
-     
-     @param es_queries - List of ES queries returned by ElasticSearch
-     
-     @return GeoJSON representation of the ES queries
+    Transforms ES results into GeoJSON. This is a helper function to be used by the geospatial_search module.
+
+    @param es_queries - List of ES queries returned by ElasticSearch
+
+    @return GeoJSON representation of the ES queries
     """
     geojson = {
         "type": "FeatureCollection",
         "name": "Ausome Maps - Therapy Centers",
-        "features": []
+        "features": [],
+        "total": {"value": 0, "relation": "eq"},
     }
     try:
         # Add features to geojson.
@@ -17,6 +18,7 @@ def transform_es_result_to_geojson(es_queries):
             data = es_query["_source"].copy()
             data["id"] = es_query["_id"]
             geojson["features"].append(data)
+        geojson["total"] = es_queries["hits"]["total"]
     except:
         pass
     return geojson
--- api/app/models/facilities.py
+++ api/app/models/facilities.py
@@ -1,217 +1,117 @@
-from enum import Enum
 from datetime import datetime
-from typing import Dict, List
+from typing import Dict, List, Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 DEFAULT_SERVICES_OFFERED = {
     "speechlanguagetherapy": {
         "label": "Speech-Language Therapy",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "speechlanguagepathology": {
         "label": "Speech-Language Pathology",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "occupationaltherapy": {
         "label": "Occupational Therapy",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "behavioraltherapy": {
         "label": "Behavioral Therapy",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "physicaltherapy": {
         "label": "Physical Therapy",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "lifeskillstraining": {
         "label": "Life Skills Training",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "socialskillstraining": {
         "label": "Social Skills Training",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "integration": {
         "label": "Integration",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "integrationprogram": {
         "label": "Integration Program",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "jobcoaching": {
         "label": "Job Coaching",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "specialeducation": {
         "label": "Special Education",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "spedtutorials": {
         "label": "SpEd Tutorials",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "parentcoaching": {
         "label": "Parent Coaching",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "educationsessionforfamilies": {
         "label": "Education Session for Families",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "feeding": {
         "label": "Feeding",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "counseling": {
         "label": "Counseling",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "psychotherapy": {
         "label": "Psychotherapy",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "abatherapy": {
         "label": "ABA Therapy",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "mnri": {
         "label": "MNRI",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "sensoryintegration": {
         "label": "Sensory Integration",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "playschool": {
         "label": "Play School",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "dysphagiamanagement": {
         "label": "Dysphagia Management",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "orthoses": {
         "label": "Orthoses (Splinting)",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "homeschoolfacilitation": {
         "label": "Homeschool Facilitation",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
     },
     "rehabconsultation": {
         "label": "Rehab Consultation",
-        "mode": {
-            "teletherapy": 0,
-            "onsite": 0,
-            "home_service": 0
-        }
-    }
+        "mode": {"teletherapy": 0, "onsite": 0, "home_service": 0},
+    },
 }
 
 DEFAULT_ACCREDITATION = {"paot": 0, "pasp": 0}
 
+
 class FaciltyProperties(BaseModel):
-    start: datetime
-    end: datetime
+    start: Optional[datetime] = Field(default_factory=datetime.now)
+    end: Optional[datetime] = Field(default_factory=datetime.now)
     info_src_name: str | None = ""
     info_src_designation: str | None = ""
     placename: str | None = ""
@@ -225,13 +125,14 @@ class FaciltyProperties(BaseModel):
     website: str | None = ""
     social_media: Dict | None = {}  # this will handle facebook and instagram urls
     services_offered: Dict | None = DEFAULT_SERVICES_OFFERED
-    other_services: Dict | None = {}
+    other_services: str | None = ""
     caters_to: List | None = []
     images: List | None = []
     osm_id: str | None = ""
     accreditation: Dict | None = DEFAULT_ACCREDITATION
 
+
 class Facilities(BaseModel):
     geometry: Dict
     properties: FaciltyProperties
-    id: int
+    id: str
