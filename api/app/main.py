b86c71f23210323fc67ceb684ae4e72f1d709ded
Ian Panganiban <ian.panganiban@outlook.ph>
2023-08-10 16:29:05 +0800

    fix: added cors headers in endpoint responses (#45)

--- api/app/main.py
+++ api/app/main.py
@@ -3,11 +3,20 @@ from fastapi import FastAPI
 from routers import geocoder, search, facilities
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
+from fastapi.middleware.cors import CORSMiddleware
 
 from redis import asyncio as aioredis
 
-app = FastAPI()
+origins = ['*']
 
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(geocoder.router)
 app.include_router(search.router)
 app.include_router(facilities.router)
