5f6bbc5054930dd415fa96eb58f76bfa12def32d
Ian Panganiban <ian.panganiban@outlook.ph>
2023-09-20 07:13:53 +0800

    docs: added typings to search/base.py and facilities/transform.py

--- api/src/app/libs/search/base.py
+++ api/src/app/libs/search/base.py
@@ -12,11 +12,12 @@ class BaseSearch:
         token: Optional[str] = None,
         search_type: Optional[str] = "search",
     ):
-        """
-        Initialize the object with the url and token. This is the constructor for the Request
+        """Initialize BaseSearch Class.
 
-        @param url - The url to request from
-        @param token - The token to use for this request if it
+        Args:
+            url (str): The URL.
+            token (Optional[str], optional): The token. Defaults to None.
+            search_type (Optional[str], optional): The search type. Defaults to "search".
         """
         self.url = url
         self.headers = {"Content-type": "application/json"}
@@ -27,11 +28,22 @@ class BaseSearch:
 
     def get(self, params: dict, url: Optional[str] = None) -> Response:
         """
-        Get data from external search endpoint. This is a wrapper around requests. get that handles status codes that aren't 200
+        Sends a GET request to the specified URL with the given parameters.
 
-        @param params - Parameters to be passed to the request
+        Parameters:
+            params (dict): The parameters to be sent with the request.
+            url (Optional[str]): The URL to send the request to. If not provided, the class URL will be used.
 
-        @return JSON response or error message if something went wrong
+        Returns:
+            Response: The response object containing the JSON string with the response.
+
+        Raises:
+            Exception: If the request fails and the status code is not 200.
+
+        Example:
+            >>> params = {"key": "value"}
+            >>> url = "https://example.com"
+            >>> response = get(params, url)
         """
         url_get = self.url
         if url is not None:
@@ -43,32 +55,49 @@ class BaseSearch:
                 "detail": f"{self.search_type} query failed. Due to {resp.json()['error']}",
                 "status_code": resp.status_code,
             }
-        data = resp.json()
-        return data
+        return resp.json()
 
-    def post(self, query: dict, url: Optional[str] = None) -> Response:
+    def post(self, data: dict, url: Optional[str] = None) -> Response:
+        """
+        Sends a POST request to the specified URL with the given query parameters and returns the response.
+
+        Args:
+            data (dict): The query parameters to send in the request.
+            url (Optional[str]): The URL to send the request to. If not specified, the default URL will be used.
+
+        Returns:
+            Response: The response from the API call, as a JSON string. If the request is unsuccessful, a dictionary with details of the failure will be returned.
+        """
         url_get = self.url
         if url is not None:
             url_get = url
-        resp = requests.get(url_get, json=query, headers=self.headers)
+        resp = requests.get(url_get, json=data, headers=self.headers)
         # Returns a JSON string with the response.
         if resp.status_code != 200:
             return {
                 "detail": f"{self.search_type} query failed. Due to {resp.json()['error']}",
                 "status_code": resp.status_code,
             }
-        data = resp.json()
-        return data
+        return resp.json()
 
     def put(self, data: dict, url: Optional[str] = None) -> Response:
         """
-        Put data to external search endpoint. This is a wrapper around requests. post that handles errors and returns a dict with status code and error message
+        Sends a PUT request to the specified URL with the provided data.
 
-        @param data - dict to be sent as POST body
+        Args:
+            data (dict): The data to be sent in the request body.
+            url (Optional[str]): The URL to send the PUT request to. If not provided, the default URL will be used.
 
-        @return dict with response from API or error message if status
+        Returns:
+            Response: The response object containing the JSON data returned by the request.
+
+        Raises:
+            dict: A dictionary with details about the failed query if the response status code is >= 400.
+
+        Example Usage:
+            >>> data = {"key": "value"}
+            >>> response = put(data, "https://example.com/api")
         """
-
         url_put = self.url
         if url is not None:
             url_put = url
@@ -80,5 +109,4 @@ class BaseSearch:
                 "detail": f"{self.search_type} query failed. Error: {resp.json()}",
                 "status_code": resp.status_code,
             }
-        data = resp.json()
-        return data
+        return resp.json()
