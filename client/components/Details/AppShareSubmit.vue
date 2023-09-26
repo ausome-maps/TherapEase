a42fc6dfb74754e2574cc12b1ea7bbe19695d609
chamthesleeptalker <4580789+chamthesleeptalker@users.noreply.github.com>
2023-09-26 14:09:20 +0800

    interface: removed share button

--- client/components/Details/AppShareSubmit.vue
+++ client/components/Details/AppShareSubmit.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="grid grid-cols-2 gap-5 my-5">
-        <div class="flex justify-center">
+    <!-- <div class="grid grid-cols-2 gap-5 my-5"> -->
+        <div class="grid my-5">
+        <!-- <div class="flex justify-center">
             <span @click="openModal(index)"
                 class="shadow-xl text-black h-[40px] w-[100%] flex items-center justify-center bg-white rounded-3xl transition-colors duration-300 hover:bg-red-400 hover:text-white cursor-pointer">
                 Share
             </span>
-        </div>
+        </div> -->
         <div class="flex justify-center">
 
             <span @click="openModal(index)"
