2bc554054268869215b4e733f874433f449558e4
Ian Panganiban <ian.panganiban@outlook.ph>
2023-09-28 08:03:53 +0800

    dev: added google analytics monitoring

--- client/components/Base/AppHeader.vue
+++ client/components/Base/AppHeader.vue
@@ -1,13 +1,4 @@
 <template>
-  <!-- Google tag (gtag.js) -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-E1HY2D8NC8"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-E1HY2D8NC8');
-  </script>
   <nav class="bg-white shadow border-gray-200 dark:bg-gray-900">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <a href='search-page' class="flex items-center">
@@ -22,6 +13,18 @@ import placeholder from "assets/images/logo_horizontal.png"
 export default {
   data() {
     return {
-      image_placeholder: placeholder}}
+      image_placeholder: placeholder
+    }
+  },
+  mounted(){
+    let recaptchaScript = document.createElement('script')
+      recaptchaScript.setAttribute('src', 'https://www.googletagmanager.com/gtag/js?id=G-E1HY2D8NC8')
+      document.head.appendChild(recaptchaScript)
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-E1HY2D8NC8');
+  }
       }
 </script>
