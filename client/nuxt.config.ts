d6adab18dda579eb0f6933aa3c1a80cc81179aed
Ian Panganiban <ian.panganiban@outlook.ph>
2023-10-02 09:31:31 +0800

    fix: missing public url to nuxt config.ts

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -4,10 +4,10 @@ export default defineNuxtConfig({
     runtimeConfig: {
         public: {
           apiURL: process.env.NUXT_PUBLIC_API_URL || "http://localhost:9001",
-          geocode: `${process.env.NUXT_PUBLIC_API_URL}/geocode` || "http://localhost:9001/geocode",
-          search: `${process.env.NUXT_PUBLIC_API_URL}/search` || "http://localhost:9001/search",
-          baseURL: `${process.env.NUXT_PUBLIC_UI_URL}/geocode` || "http://localhost:9002",
-          facilities:  `${process.env.NUXT_PUBLIC_API_URL}/facilities` || "http://localhost:9001/facilities",
+          geocode: process.env.NUXT_PUBLIC_API_URL + "/geocode" || "http://localhost:9001/geocode",
+          search: process.env.NUXT_PUBLIC_API_URL + "/search" || "http://localhost:9001/search",
+          baseURL: process.env.NUXT_PUBLIC_UI_URL || "http://localhost:9002",
+          facilities:  process.env.NUXT_PUBLIC_API_URL + "facilities" || "http://localhost:9001/facilities",
           feedbackUrl: process.env.FEEDBACK_FORM,
           googleTagManager: process.env.GOOGLE_TAG_MANAGER || "https://www.googletagmanager.com/gtag/js?id=G-E1HY2D8NC8", 
         }
