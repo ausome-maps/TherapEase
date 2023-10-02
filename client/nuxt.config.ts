afc958ccad38e439c16151f0c137ef8c82349be9
Ian Panganiban <ian.panganiban@outlook.ph>
2023-10-02 08:41:23 +0800

    fix: localhost configs in details page and updated configs to use environment variables

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -3,11 +3,11 @@ export default defineNuxtConfig({
   ssr: false,
     runtimeConfig: {
         public: {
-          apiURL: process.env.NUXT_API_URL || "http://localhost:9001",
-          geocode: "http://localhost:9001/geocode",
-          search: "http://localhost:9001/search",
-          baseURL: "http://localhost:9002",
-          facilities:  "http://localhost:9001/facilities",
+          apiURL: process.env.NUXT_PUBLICAPI_URL || "http://localhost:9001",
+          geocode: `${process.env.NUXT_PUBLICAPI_URL}/geocode` || "http://localhost:9001/geocode",
+          search: `${process.env.NUXT_PUBLICAPI_URL}/search` || "http://localhost:9001/search",
+          baseURL: `${process.env.NUXT_PUBLICUI_URL}/geocode` || "http://localhost:9002",
+          facilities:  `${process.env.NUXT_PUBLIC_API_URL}/facilities` || "http://localhost:9001/facilities",
           feedbackUrl: process.env.FEEDBACK_FORM,
           googleTagManager: process.env.GOOGLE_TAG_MANAGER || "https://www.googletagmanager.com/gtag/js?id=G-E1HY2D8NC8", 
         }
