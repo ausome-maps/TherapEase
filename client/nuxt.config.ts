48c28910f288b374946c9bbf39faeb4a2676c516
lkpanganiban <ian.panganiban@outlook.ph>
2023-11-03 15:18:14 +0800

    dev: hardcoded feedback url form

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -8,7 +8,7 @@ export default defineNuxtConfig({
           searchURL: process.env.NUXT_PUBLIC_API_URL + "/search" || "http://localhost:9001/search",
           baseURL: process.env.NUXT_PUBLIC_UI_URL || "http://localhost:9002",
           facilitiesURL:  process.env.NUXT_PUBLIC_API_URL + "/facilities" || "http://localhost:9001/facilities",
-          feedbackURL: process.env.NUXT_PUBLIC_FEEDBACK_URL,
+          feedbackURL: process.env.NUXT_PUBLIC_FEEDBACK_URL || "https://forms.gle/Yo4zMgBXXfAL7vyS6",
           googleTagManager: process.env.GOOGLE_TAG_MANAGER || "https://www.googletagmanager.com/gtag/js?id=G-E1HY2D8NC8", 
         }
       },
