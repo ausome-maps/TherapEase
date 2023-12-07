76d87e5b252c22de789ce0ee195936170c57d08c
Ian Panganiban <ian.panganiban@outlook.ph>
2023-12-07 19:36:33 +0800

    chore: added whitenoise for prod with traefik

--- django/django_app/settings.py
+++ django/django_app/settings.py
@@ -76,6 +76,7 @@ AUTHENTICATION_BACKENDS = (
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # new
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -146,11 +147,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-if DEBUG:
-    CORS_ORIGIN_ALLOW_ALL = True
+# if DEBUG:
+CORS_ORIGIN_ALLOW_ALL = True
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+    STATIC_ROOT = BASE_DIR / "static"
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
