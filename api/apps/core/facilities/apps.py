354069d55978077cde62118cd78bf3ae3f3fc84e
Ian Panganiban <ian.panganiban@outlook.ph>
2023-12-19 13:53:11 +0800

    fix: creation of facilities and facilityproperties using admin

--- api/apps/core/facilities/apps.py
+++ api/apps/core/facilities/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class FacilitiesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "apps.core.facilities"
+
+    def ready(self):
+        import apps.core.facilities.signals
